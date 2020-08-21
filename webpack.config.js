b6c8af2e72bda3b343bd813d5b5fbb66969f8c3e
Carlos Eduardo Porto <69756514+carloseduardoporto@users.noreply.github.com>
2020-08-21 14:54:00 -0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' }, // ele vai pegar o css que foi interpretado pelo css-loader e colocar dentro do html
+          { loader: 'css-loader' }, // ele lê o arquivo css e conseguir interpretar as importaçoes e vai estar pronto
+        ]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: {
+          loader: 'file-loader',
+        }
+      }
+    ]
+  },
+};
